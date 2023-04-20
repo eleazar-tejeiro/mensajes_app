0da9655eb4f706adafd23bc63da29b4326f99543
Uriel Tejeiro <uriel.tejeiro@oktana.com>
2023-04-19 20:08:55 -0400

    ✨ feat(users): add user creation, listing, editing, and login functionality
    This commit adds functionality to create, list, edit, and login users. The
    UserService class now includes methods to create a user, list all users, edit a
    user, and authenticate a user. The UserDAO class includes methods to create a
    user in the database, list all users from the database, edit a user in the
    database, and authenticate a user from the database. These changes allow users
    to be managed in the messaging app.

--- src/main/java/com/platzi/mensajes_app/dao/UserDAO.java
+++ src/main/java/com/platzi/mensajes_app/dao/UserDAO.java
@@ -1,4 +1,115 @@
 package com.platzi.mensajes_app.dao;
 
+import com.platzi.database.DbConnection;
+import com.platzi.mensajes_app.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
+    static String cantCreateUser = "\n no se pudo crear el usuario \n";
+    static String createdUser = "\n usuario creado, ahora puedes iniciar sesión \n";
+    static String cantListUsers = "\n no se pudo listar los usuarios \n";
+    static String updatedUser = "\n usuario actualizado \n";
+    static String cantUpdateUser = "\n no se pudo actualizar el usuario \n";
+    static String loginFailed = "login failed";
+    static String dbAuthFailed = "\n no se pudo autenticar con el servidor \n";
+
+    public static void createUserOnDB(User usuario) {
+        DbConnection dbConnection = DbConnection.getInstance();
+
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement ps = null;
+            try {
+                String query = "insert into usuarios(correo,clave,nombre_completo) values (?,?,?)";
+                ps = connection.prepareStatement(query);
+                ps.setString(1, usuario.getEmail());
+                ps.setString(2, usuario.getPassword());
+                ps.setString(3, usuario.getFullName());
+                ps.executeUpdate();
+                System.out.println(createdUser);
+            } catch (SQLException e) {
+                System.out.println(cantCreateUser);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void listUsersOnDB() {
+        DbConnection dbConnection = DbConnection.getInstance();
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try {
+                String query = "select * from usuarios";
+                ps = connection.prepareStatement(query);
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    System.out.print("\n[ID: " + rs.getString("id_usuario") + " | ");
+                    System.out.print("Correo: -" + rs.getString("correo") + " | ");
+                    System.out.print("Nombre: " + rs.getString("nombre_completo") + " ] ");
+                }
+            } catch (SQLException e) {
+                System.out.println(cantListUsers);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void editUserOnDB(User usuario) {
+        DbConnection dbConnection = DbConnection.getInstance();
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement ps = null;
+            try {
+                String query = "update usuarios set correo = ?, clave = ?, nombre_completo = ? where id_usuario = ?";
+                ps = connection.prepareStatement(query);
+                ps.setString(1, usuario.getEmail());
+                ps.setString(2, usuario.getPassword());
+                ps.setString(3, usuario.getFullName());
+                ps.setInt(4, usuario.getUserId());
+                ps.executeUpdate();
+                System.out.println(updatedUser);
+            } catch (SQLException e) {
+                System.out.println(cantUpdateUser);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static User loginDB(User usuario) {
+        DbConnection dbConnection = DbConnection.getInstance();
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try {
+                String query = "select * from usuarios where correo=? and clave=?";
+                ps = connection.prepareStatement(query);
+                ps.setString(1, usuario.getEmail());
+                ps.setString(2, usuario.getPassword());
+                rs = ps.executeQuery();
+                User login = new User();
+                if (rs.next()) {
+                    System.out.println("login correcto!");
+                    login.setUserId(rs.getInt("id_usuario"));
+                    login.setEmail(rs.getString("correo"));
+                    login.setFullName(rs.getString("nombre_completo"));
+                } else {
+                    System.out.println(loginFailed);
+                }
+                //retornamos el objeto usuario con los datos o vacío
+                return login;
+            } catch (SQLException e) {
+                System.out.println(dbAuthFailed);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        //si no se logra iniciar sesión devolvemos un objeto vacío
+        return null;
+    }
 }
--- src/main/java/com/platzi/mensajes_app/service/UserService.java
+++ src/main/java/com/platzi/mensajes_app/service/UserService.java
@@ -1,4 +1,63 @@
 package com.platzi.mensajes_app.service;
 
+import com.platzi.mensajes_app.dao.UserDAO;
+import com.platzi.mensajes_app.model.User;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Scanner;
+
 public class UserService {
+    static String emailMessage = "indica tu email";
+    static String passwordMessage = "indica tu password";
+    static String fullNameMessage = "indica tu nombre completo";
+
+
+    public static void createUser(Scanner sc) {
+        System.out.println(fullNameMessage);
+        String fullName = sc.nextLine();
+        System.out.println(emailMessage);
+        String email = sc.nextLine();
+        System.out.println(passwordMessage);
+        String password = sc.nextLine();
+
+        password = getMd5Hash(password);
+
+        User newUser = new User(0, email, password, fullName);
+        UserDAO.createUserOnDB(newUser);
+    }
+
+    public static void listUsers() {
+        UserDAO.listUsersOnDB();
+    }
+
+    public static void editUser(User usuario, Scanner sc) {
+        System.out.println(fullNameMessage);
+        String fullName = sc.nextLine();
+        System.out.println(emailMessage);
+        String email = sc.nextLine();
+        System.out.println(passwordMessage);
+        String password = sc.nextLine();
+
+        String passwordHashed = getMd5Hash(password);
+        User updatedUser = new User(usuario.getUserId(), email, passwordHashed, fullName);
+        UserDAO.editUserOnDB(updatedUser);
+    }
+
+    public static String getMd5Hash(String password) {
+        return DigestUtils.md5Hex(password);
+    }
+
+    public static User login(Scanner sc) {
+        //create an instance of Scanner class to read user input
+        System.out.println(emailMessage);
+        String email = sc.next();
+        System.out.println(passwordMessage);
+        String password = sc.next();
+
+        String passwordHashed = getMd5Hash(password);
+
+        User login = new User(email, passwordHashed);
+        User result = UserDAO.loginDB(login);
+        return result;
+    }
 }
